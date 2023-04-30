c4e048e8965fa429fe4bb8fbd3ec58b85fdcb3db
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-04-30 21:20:29 +0800

    Add eventtap and sponsor utility functions

---
+++ lib/eventtap_utils.lua
@@ -0,0 +1,14 @@
+--[[ Functions - API calls offered directly by the extension
+checkKeyboardModifiers
+checkMouseButtons
+doubleClickInterval
+isSecureInputEnabled
+keyRepeatDelay
+keyRepeatInterval
+keyStroke
+keyStrokes
+leftClick
+middleClick
+otherClick
+rightClick
+scrollWheel ]]
