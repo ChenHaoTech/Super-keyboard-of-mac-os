deac9800151197bd2e5556e608522c5da8ed901f
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-04-30 20:55:05 +0800

    Add dialog_utils module and cleanup code in shortcut_utils

---
+++ lib/dialog_utils.lua
@@ -0,0 +1,16 @@
+--[[ -- 显示文本输入对话框，获取用户输入的文本内容
+local result, text = hs.dialog.textPrompt("请输入一段文本", "这是默认文本")
+
+-- 如果用户输入了文本，打印文本内容
+if result == "OK" then
+    print(text)
+end
+
+-- 显示文件选择对话框，获取用户选择的文件或文件夹路径
+local result, files = hs.dialog.chooseFileOrFolder("请选择文件或文件夹", "~/Documents", false, true, false)
+
+-- 如果用户选择了文件或文件夹，打印路径
+if result == "OK" then
+    print(files[1])
+end
+ ]]
--- lib/shortcut_utils.lua
+++ lib/shortcut_utils.lua
@@ -1,5 +1,1 @@
 -- hs.shortcuts.list()
-
---[[ 
-
- ]]
